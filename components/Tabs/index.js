4b0eacce1dd2cf16e0e1e3e24d4a5713b0bcb97b
Robert Ramos Jr <Robert.ramosjr214@gmail.com>
2019-08-16 20:10:21 -0500

    Updated header/index.js and tab/index.js files

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,27 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(response => {
+        let topicsElement = document.querySelector(".topics");
+        topicsElement.appendChild(createTopics(response.data.topics));
+    })
+    .catch(error => {
+        console.error(error);
+    })
+
+function createTopics(data) {
+
+    let trends = [];
+    let topics = document.querySelector(".topics");
+    for (let i = 0; i < data.length; i++){
+        trends.push(document.createElement("div"));
+    }
+    for (let i = 0; i < trends.length; i++){
+        trends[i].textContent = data[i];
+        trends[i].classList.add("tab");
+        topics.appendChild(trends[i]);
+    }
+    return topics;
+}
